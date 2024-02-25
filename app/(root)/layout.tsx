3b4a528d3dedd6e418a55cfcfe8ee7d20f6d72aa
Jorge Matos <jisapjisap@gmail.com>
2024-02-25 18:52:42 +0100

    sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,13 @@
-
-
+import Sidebar from "@/components/Shared/Sidebar"
 
 
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
+      <Sidebar />
+      {/* MobileNav */}
+
       <div className="root-container">
         <div className="wrapper">
           {children}
---
+++ components/Shared/Sidebar.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image"
+import Link from "next/link"
+
+
+const Sidebar = () => {
+  return (
+    <aside className="sidebar">
+      <div className="flex size-full flex-col gap-4">
+        <Link
+          href="/"
+          className="sidebar-logo"
+        >
+          <Image 
+            src="/assets/images/logo-text.svg"
+            alt="logo"
+            width={180}
+            height={28}
+          />
+        </Link>
+      </div>
+    </aside>
+  )
+}
+
+export default Sidebar
