52fc9adbae5e7516f072cb429218f8fa65966582
Jorge Matos <jisapjisap@gmail.com>
2024-02-25 20:01:15 +0100

    sidebar

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ['/api/webhooks/clerk', '/api/webhooks/stripe'] //Rutas sin autenticación
+  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe'] //Rutas sin autenticación
 });
 
 export const config = {
